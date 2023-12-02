5fedcce346a5ee054bd5267ca307ee220a9436c0
Tsz Kit Cheung <115593395+kitkit922@users.noreply.github.com>
2023-12-01 22:52:29 -0500

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Tsz Kit Cheung on 2023-12-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
